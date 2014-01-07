06cc2d5d1348f4ef79e1d4e4886b2e0245067ca3
ondrej <ondrejh.ck@email.cz>
2014-01-07 10:43:16 +0100

    ifndef statement

--- servo.h
+++ servo.h
@@ -3,6 +3,9 @@
  * see *.c file for details
  **/
 
+#ifndef _SERVO_H_
+#define _SERVO_H_
+
 #define SERVO_CLOCK 20000
 #define SERVO_CENTER 1500
 #define SERVO_MAX 500
@@ -10,3 +13,5 @@
 void set_servo_position(int pos);
 int get_servo_position(void);
 void init_servo(int pos);
+
+#endif
