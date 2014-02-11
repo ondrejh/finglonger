1c39e1a2209be1fe1ff1e2e42d973607d956da64
Ondřej Hejda <ondrejh.ck@email.cz>
2014-02-11 22:53:24 +0100

    timing function

--- main.c
+++ main.c
@@ -15,11 +15,30 @@
 #define BTN_CENTER ((PINC&0x02)==0)
 #define BTN_LEFT   ((PINC&0x04)==0)
 
+#define BTN_MINUTE ((PINC&0x01)==0)
+#define BTN_HOUR   ((PINC&0x04)==0)
+
+#define MAX_HOURS 12
+#define CNT_SET_TIMEOUT 250 //5s
+
 void use_force(int8_t data[4],uint8_t force)
 {
     uint8_t i;
     for (i=0;i<4;i++) if (force>i) data[i]=0x08; else data[i]=0x7F;
 }
+
+void use_time(int8_t data[4],uint8_t hours, uint8_t minutes)
+{
+    data[0]=hours/10;
+    data[1]=hours%10;
+    data[2]=minutes/10;
+    data[3]=minutes%10;
+}
+
+#define SEQV_RESET 0
+#define SEQV_PUSH 10
+#define SEQV_SET_TIME 20
+#define SEQV_COUNT_TIME 30
 
 /** main program body */
 int main(void)
@@ -40,6 +59,8 @@ int main(void)
     // interrupt enable
     sei();
 
+    // time
+    int8_t hours=0,minutes=0,seconds=0;
     // display test
     int8_t data[4]={0,0,0,0};
     /*disp_point(true);
@@ -61,19 +82,28 @@ int main(void)
             uint8_t s;
             switch (seqv)
             {
-                case 0: // start
+                case SEQV_RESET: // start
                     set_servo_position(500);
                     disp_clearDisplay();
+                    hours=0;
+                    minutes=0;
+                    seconds=0;
                     seqv++;
                     break;
-                case 1: // wait test or clock button
-                    if (BTN_CENTER)
+                case (SEQV_RESET+1): // wait buttons released
+                    if ((!BTN_MINUTE) && (!BTN_HOUR))
                     {
-                        seqv++;
+                        cnt++;
+                        if (cnt>=5) seqv++;
                     }
+                    else cnt=0;
                     break;
-                case 2: // move down until -500 or force 4
+                case (SEQV_RESET+2): // wait buttons
+                    if (BTN_HOUR || BTN_MINUTE) seqv=SEQV_SET_TIME;
+                    break;
+                case (SEQV_PUSH): // move down until -500 or force 4
                     s = sens_read();
+                    disp_point(false);
                     use_force(data,s);
                     disp_displayAll(data);
                     set_servo_position(get_servo_position()-(4-s)*10);
@@ -83,9 +113,9 @@ int main(void)
                         seqv++;
                     }
                     break;
-                case 3: // wait a while with force 3
+                case (SEQV_PUSH+1): // wait a while with force 3
                     cnt++;
-                    if (cnt>100)
+                    if (cnt>25)
                     {
                         seqv++;
                     }
@@ -98,15 +128,127 @@ int main(void)
                         if (s<3) set_servo_position(get_servo_position()-(4-s));
                     }
                     break;
-                case 4: // move back to top
+                case (SEQV_PUSH+2): // move back to top
                     s = sens_read();
                     use_force(data,s);
                     disp_displayAll(data);
                     set_servo_position(get_servo_position()+40);
                     if (get_servo_position()>=500) seqv=0;
                     break;
+                case (SEQV_SET_TIME): // setup clock timer
+                    disp_point(true);
+                    seconds=0;
+                    cnt=0;
+                    seqv++;
+                    break;
+                case (SEQV_SET_TIME+1): // wait btn press
+                    cnt++;
+                    if (BTN_MINUTE)
+                    {
+                        minutes++;
+                        if (minutes>=60) minutes=0;
+                        seqv++;
+                        cnt=0;
+                    }
+                    else if(BTN_HOUR)
+                    {
+                        hours++;
+                        if (hours>=MAX_HOURS) hours=0;
+                        seqv++;
+                        cnt=0;
+                    }
+                    else if (cnt>CNT_SET_TIMEOUT)
+                    {
+                        if ((hours==0) && (minutes==0)) minutes=1;
+                        seqv=SEQV_COUNT_TIME;
+                    }
+                    use_time(data,hours,minutes);
+                    disp_displayAll(data);
+                    break;
+                case (SEQV_SET_TIME+2): // wait btn release
+                    if ((!BTN_MINUTE) && (!BTN_HOUR))
+                    {
+                        cnt++;
+                        if (cnt>=5) seqv=SEQV_SET_TIME;
+                    }
+                    else
+                    {
+                        if (BTN_MINUTE&&BTN_HOUR) seqv++;
+                        cnt=0;
+                    }
+                    break;
+                case (SEQV_SET_TIME+3): // both buttons on (reset?)
+                    if (BTN_MINUTE&&BTN_HOUR)
+                    {
+                        if ((cnt%10)==0)
+                        {
+                            if (cnt/10%2) disp_clearDisplay();
+                            else disp_displayAll(data);
+                        }
+                        cnt++;
+                        if (cnt>=100) seqv=SEQV_RESET;
+                    }
+                    else seqv--;
+                    break;
+                case (SEQV_COUNT_TIME): // countdown
+                    cnt=1;
+                    disp_point(true);
+                    if (hours!=0) use_time(data,hours,minutes);
+                    else use_time(data,minutes,seconds);
+                    disp_displayAll(data);
+                    seqv++;
+                    break;
+                case (SEQV_COUNT_TIME+1): // wait half second
+                    cnt++;
+                    if (BTN_HOUR || BTN_MINUTE) seqv=SEQV_SET_TIME;
+                    else if (cnt>=25)
+                    {
+                        disp_point(false);
+                        disp_displayAll(data);
+                        seqv++;
+                    }
+                    break;
+                case (SEQV_COUNT_TIME+2): // wait the rest of second
+                    cnt++;
+                    if (BTN_HOUR || BTN_MINUTE) seqv=SEQV_SET_TIME;
+                    else if (cnt>=50)
+                    {
+                        seconds--;
+                        if (seconds<0)
+                        {
+                            seconds=59;
+                            minutes--;
+                            if (minutes<0)
+                            {
+                                minutes=59;
+                                hours--;
+                            }
+                        }
+                        if ((seconds==0)&&(minutes==0)&&(hours==0))
+                        {
+                            cnt=0;
+                            disp_point(true);
+                            use_time(data,0,0);
+                            seqv++;
+                        }
+                        else seqv=SEQV_COUNT_TIME;
+                    }
+                    break;
+                case (SEQV_COUNT_TIME+3): // start countdown last second
+                    if (BTN_HOUR || BTN_MINUTE) seqv=SEQV_SET_TIME;
+                    else
+                    {
+                        if ((cnt%10)==0)
+                        {
+                            if (cnt/10%2) disp_clearDisplay();
+                            else disp_displayAll(data);
+                        }
+                        cnt++;
+                        if (cnt>=50) seqv=SEQV_PUSH;
+                    }
+                    break;
                 default:
-                    seqv=0;
+                    seqv=SEQV_RESET;
                     break;
             }
             /*static uint8_t cnt = 0;
